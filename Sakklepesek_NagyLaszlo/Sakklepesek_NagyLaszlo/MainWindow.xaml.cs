bd4a6a889b5e4fac3891f9a3e480c52d6a10eefd
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2022-05-26 20:55:23 +0200

    A tábla kialakítása
    Canvasra felrakja egy méret szerint a Rectangle-ket és mint egy sakktábla olyan mintázatú a tábla.

--- Sakklepesek_NagyLaszlo/Sakklepesek_NagyLaszlo/MainWindow.xaml.cs
+++ Sakklepesek_NagyLaszlo/Sakklepesek_NagyLaszlo/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace Sakklepesek_NagyLaszlo
     /// </summary>
     public partial class MainWindow : Window
     {
+        int meret = 8;
+        Rectangle[,] mezok;
         public MainWindow()
         {
             InitializeComponent();
+            TablaLetrehozasa();
+        }
+
+        private void TablaLetrehozasa()
+        {
+            tabla.Children.Clear();
+            mezok = new Rectangle[meret, meret];
+            for (int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    Rectangle negyzet = new Rectangle();
+                    negyzet.Width = tabla.Width / meret;
+                    negyzet.Height = tabla.Height / meret;
+                    negyzet.Margin = new Thickness(tabla.Width / meret * j, tabla.Height / meret * i, 0, 0);
+
+                    tabla.Children.Add(negyzet);
+                    mezok[i, j] = negyzet;
+                }
+            }
+            for (int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    
+                    if (j % 2 == 1 && i % 2 == 1)
+                    {
+                        mezok[i, j].Fill = Brushes.SaddleBrown;
+                    }
+                    else if (j % 2 == 0 && i % 2 == 0)
+                    {
+                        mezok[i, j].Fill = Brushes.SaddleBrown;
+                    }
+                    else
+                    {
+                        mezok[i, j].Fill = Brushes.Wheat;
+                    }
+                }
+            }
         }
     }
 }
