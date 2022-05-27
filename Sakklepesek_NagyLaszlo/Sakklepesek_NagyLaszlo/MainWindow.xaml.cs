cee89950d28a837b68083ef415081ae903d09148
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2022-05-27 11:20:47 +0200

    Az első bábú
    Sok-sok hibakód és próbálkozás után sikerült megcsinálni és megjelent az első bábú. A színezés nem tökéletes az Fix-re vár. A mozgást illetően bárhová tud lépni, úgyhogy ezt is fixelni kell majd.

--- Sakklepesek_NagyLaszlo/Sakklepesek_NagyLaszlo/MainWindow.xaml.cs
+++ Sakklepesek_NagyLaszlo/Sakklepesek_NagyLaszlo/MainWindow.xaml.cs
@@ -21,7 +21,9 @@ namespace Sakklepesek_NagyLaszlo
     public partial class MainWindow : Window
     {
         int meret = 8;
-        Rectangle[,] mezok;
+        Button[,] mezok;
+        Babu currentBabu;
+        //Babu feherGyalog;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,20 +36,46 @@ namespace Sakklepesek_NagyLaszlo
             babuComboBox.Items.Add("Futó");
             babuComboBox.Items.Add("Király");
             babuComboBox.Items.Add("Királynő");
+
+            List<int> kezdoHely = new List<int>();
+            kezdoHely.Add(3);
+            kezdoHely.Add(2);
+
+            List<List<int>> lepesekLista = new List<List<int>>();
+            List<int> lepes1 = new List<int>();
+            lepes1.Add(1);
+            lepes1.Add(1);
+
+            List<int> lepes2 = new List<int>();
+            lepes2.Add(2);
+            lepes2.Add(1);
+
+            lepesekLista.Add(lepes1);
+            lepesekLista.Add(lepes2);
+
+            /*Fehér gyalog
+            List<int> kezdoHely = new List<int>();
+            kezdoHely.Add(1);
+            kezdoHely.Add(1);*/
+
+            currentBabu = new Babu(mezok, kezdoHely, lepesekLista, "\u2659");
+            szinezGombokat(currentBabu.odaLep);
         }
 
         private void TablaLetrehozasa()
         {
             tabla.Children.Clear();
-            mezok = new Rectangle[meret, meret];
+            mezok = new Button[meret, meret];
             for (int i = 0; i < meret; i++)
             {
                 for (int j = 0; j < meret; j++)
                 {
-                    Rectangle negyzet = new Rectangle();
+                    Button negyzet = new Button();
                     negyzet.Width = tabla.Width / meret;
                     negyzet.Height = tabla.Height / meret;
                     negyzet.Margin = new Thickness(tabla.Width / meret * j, tabla.Height / meret * i, 0, 0);
+
+                    negyzet.Click += Mozgatas;
 
                     tabla.Children.Add(negyzet);
                     mezok[i, j] = negyzet;
@@ -57,21 +85,114 @@ namespace Sakklepesek_NagyLaszlo
             {
                 for (int j = 0; j < meret; j++)
                 {
-                    
+
                     if (j % 2 == 1 && i % 2 == 1)
                     {
-                        mezok[i, j].Fill = Brushes.SaddleBrown;
+                        mezok[i, j].Background = Brushes.SaddleBrown;
                     }
                     else if (j % 2 == 0 && i % 2 == 0)
                     {
-                        mezok[i, j].Fill = Brushes.SaddleBrown;
+                        mezok[i, j].Background = Brushes.SaddleBrown;
                     }
                     else
                     {
-                        mezok[i, j].Fill = Brushes.Wheat;
+                        mezok[i, j].Background = Brushes.Wheat;
                     }
                 }
             }
+        }
+
+        public void tablaUjraSzin()
+        {
+            for (int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    if (j % 2 == 1 && i % 2 == 1)
+                    {
+                        mezok[i, j].Background = Brushes.SaddleBrown;
+                    }
+                    else if (j % 2 == 0 && i % 2 == 0)
+                    {
+                        mezok[i, j].Background = Brushes.SaddleBrown;
+                    }
+                    else
+                    {
+                        mezok[i, j].Background = Brushes.Wheat;
+                    }
+                }
+            }
+        }
+
+        public class Babu
+        {
+            public Button currentButton;
+            public string UniCode;
+            Button[,] mezok;
+            public List<List<int>> lepesekLista;
+            public List<List<int>> odaLep;
+
+
+            public Babu(Button[,] mezok, List<int> kezdoHely, List<List<int>> lepesekLista, string UniCode)
+            {
+                this.mezok = mezok;
+                this.lepesekLista = lepesekLista;
+                currentButton = mezok[kezdoHely[0], kezdoHely[1]];
+                this.UniCode = UniCode;
+                odaLep = new List<List<int>>();
+
+                for (int i = 0; i < lepesekLista.Count; i++)
+                {
+                    List<int> helyzetek = new List<int>();
+                    helyzetek.Add(kezdoHely[0] + lepesekLista[i][0]);
+                    helyzetek.Add(kezdoHely[1] + lepesekLista[i][1]);
+                    odaLep.Add(helyzetek);
+                }
+            }
+
+            public void Mozgas(List<int> kovihely)
+            {
+                currentButton.Content = "";
+
+                currentButton = mezok[kovihely[0], kovihely[1]];
+                currentButton.Content = UniCode;
+            }
+
+        }
+
+        public void szinezGombokat(List<List<int>> lepesekLista)
+        {
+            foreach (var helyzet in lepesekLista)
+            {
+                mezok[helyzet[0], helyzet[1]].Background = Brushes.LightGray;
+            }
+        }
+
+
+        private void Mozgatas(object sender, RoutedEventArgs e)
+        {
+            currentBabu.Mozgas(Index((Button)sender));
+            szinezGombokat(currentBabu.odaLep);
+            tablaUjraSzin();
+        }
+
+        private List<int> Index(Button gomb)
+        {
+            for (int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    if (mezok[i, j] == gomb)
+                    {
+                        List<int> indexek = new List<int>();
+                        indexek.Add(i);
+                        indexek.Add(j);
+
+                        return indexek;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
